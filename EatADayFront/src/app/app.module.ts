aa6c9ccbdb202d8436daf8080d63056d12a3e0e5
alvaro hernandez <alvarohernandezmurcia@gmail.com>
2023-02-15 19:02:49 +0100

    loginALvaro

--- EatADayFront/src/app/app.module.ts
+++ EatADayFront/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LogginFormComponent } from './loggin-form/loggin-form.component';
+import {ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LogginFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
