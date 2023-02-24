f92ef08077d726b6f4f4f8b2b6f7210f7f3be1fd
Claudia <claudia2daw@gmail.com>
2023-02-24 19:58:40 +0100

    Nav Icons1 & LOGOTYPE Claudia

--- EatADayFront/src/app/header/header.component.ts
+++ EatADayFront/src/app/header/header.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
 
+  public showModals: boolean = false;
+
+
+  public getModals(): void {
+    this.showModals = !this.showModals;
+  }
 }
